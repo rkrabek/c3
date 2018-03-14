33e61c05550467426749a60960a1ac8d96324aac
Robert Krabek <rokrabek@microsoft.com>
2018-03-13 17:38:23 -0700

    Fixed code to fix tests

--- src/shape.bar.js
+++ src/shape.bar.js
@@ -52,11 +52,13 @@ c3_chart_internal_fn.redrawBar = function (drawBar, withTransition) {
     ];
 };
 c3_chart_internal_fn.getBarW = function (axis, barTargetsNum) {
-    var $$ = this, config = $$.config;
-    var maxUniqueXValues = $$.data.targets.reduce(function returnMaxLength(maxLength, target) {
-        var targetLength = target.value.length;
-        return Math.max(maxLength, targetLength);
-    });
+    var $$ = this, config = $$.config, maxUniqueXValues;
+    if ($$.data.targets.length > 0) {
+        maxUniqueXValues = $$.data.targets.reduce(function returnMaxLength(maxLength, target) {
+            var targetLength = target.values.length;
+            return Math.max(maxLength, targetLength);
+        }, 0);
+    }
     var w = typeof config.bar_width === 'number' ? config.bar_width : barTargetsNum ? (axis.tickInterval(maxUniqueXValues) * config.bar_width_ratio) / barTargetsNum : 0;
     return config.bar_width_max && w > config.bar_width_max ? config.bar_width_max : w;
 };
